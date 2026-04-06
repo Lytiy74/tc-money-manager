d1fef0488efa76fb9cba468a4f72de211469dcf0
Andrii Zaika <lytiy7404@gmail.com>
2026-04-06 09:52:28 +0200

    add stricter email validation in UpdateEmailRequestDto with @NotBlank

--- src/main/java/org/tc/mtracker/auth/dto/UpdateEmailRequestDto.java
+++ src/main/java/org/tc/mtracker/auth/dto/UpdateEmailRequestDto.java
@@ -2,11 +2,13 @@ package org.tc.mtracker.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Update user email request")
 public record UpdateEmailRequestDto(
         @Schema(description = "New email address")
         @Email
+        @NotBlank
         String email
 ) {
 }
